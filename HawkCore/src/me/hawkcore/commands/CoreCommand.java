d06fb742f8834ecbefdb3e0550050153a7180fe8
lHawkOficial <luanvictorchagas2015@gmail.com>
2023-02-02 16:27:42 -0300

    updated

--- HawkCore/src/me/hawkcore/commands/CoreCommand.java
+++ HawkCore/src/me/hawkcore/commands/CoreCommand.java
@@ -5,13 +5,11 @@ import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.hawkcore.Core;
@@ -26,22 +24,6 @@ public class CoreCommand implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender s, Command c, String lb, String[] args) {
 		if (!s.hasPermission("*")) return false;
-		if (args.length == 2) {
-			if (args[0].equalsIgnoreCase("reload")) {
-				Plugin plugin = Bukkit.getPluginManager().getPlugin(args[1]);
-				try {
-					PluginManager manager = Bukkit.getPluginManager();
-					manager.disablePlugin(plugin);
-					manager.loadPlugin(getPluginFile(plugin));
-					manager.enablePlugin(plugin);
-					Bukkit.getConsoleSender().sendMessage("§aPlugin §f" + plugin.getName() + " §arecarregado com sucesso!");
-					return false;
-				} catch (Exception e) {
-					s.sendMessage("§cOcorreu um erro ao tentar recarregar este plugin!");
-					return false;
-				}
-			}
-		}
 		s.sendMessage(" ");
 		s.sendMessage(Core.getInstance().getTag() + " §7Atualização do Plugin " + Core.getInstance().getVersion());
 		s.sendMessage("§7Tasks em execução: §e" + TaskManager.get().getTasks().size());
@@ -51,7 +33,7 @@ public class CoreCommand implements CommandExecutor {
 		return false;
 	}
 	
-	private static File getPluginFile(Plugin plugin) {
+	public static File getPluginFile(Plugin plugin) {
         Method method;
         try {
             method = JavaPlugin.class.getDeclaredMethod("getFile");
