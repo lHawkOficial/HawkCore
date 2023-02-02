c8b994370d641b44f11755a15487ca59072e6945
lHawkOficial <luanvictorchagas2015@gmail.com>
2023-02-02 16:08:21 -0300

    updated

--- HawkCore/src/me/hawkcore/commands/CoreCommand.java
+++ HawkCore/src/me/hawkcore/commands/CoreCommand.java
@@ -1,10 +1,15 @@
 package me.hawkcore.commands;
 
 
+import java.io.File;
+
+
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
 
 import me.hawkcore.Core;
 import me.hawkcore.tasks.TaskManager;
@@ -18,6 +23,20 @@ public class CoreCommand implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender s, Command c, String lb, String[] args) {
 		if (!s.hasPermission("*")) return false;
+		if (args.length == 2) {
+			if (args[0].equalsIgnoreCase("reload")) {
+				Plugin plugin = Bukkit.getPluginManager().getPlugin(args[1]);
+				try {
+					Bukkit.getPluginManager().disablePlugin(plugin);
+					Bukkit.getPluginManager().loadPlugin(new File(plugin.getName() + ".jar"));
+					Bukkit.getConsoleSender().sendMessage("§aPlugin §f" + plugin.getName() + " §arecarregado com sucesso!");
+					return false;
+				} catch (Exception e) {
+					s.sendMessage("§cOcorreu um erro ao tentar recarregar este plugin!");
+					return false;
+				}
+			}
+		}
 		s.sendMessage(" ");
 		s.sendMessage(Core.getInstance().getTag() + " §7Atualização do Plugin " + Core.getInstance().getVersion());
 		s.sendMessage("§7Tasks em execução: §e" + TaskManager.get().getTasks().size());
